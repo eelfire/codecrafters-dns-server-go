cbac22d734b817bc6186cfded9a8b3e7da263e26
Tirth Patel <tirth1093@gmail.com>
2023-12-09 23:24:57 +0530

    pass 2nd stage

--- app/main.go
+++ app/main.go
@@ -9,6 +9,8 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
+	// fmt.Println(GenDnsHeaderResponse(NewDnsHeader()))
+
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
 		fmt.Println("Failed to resolve UDP address:", err)
@@ -35,7 +37,8 @@ func main() {
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
 		// Create an empty response
-		response := []byte{}
+		response := GenDnsHeaderResponse(NewDnsHeader())
+		// response := []byte{}
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
@@ -43,3 +46,39 @@ func main() {
 		}
 	}
 }
+
+type DnsHeader struct {
+	id      uint16  // 16 bits
+	flags   [2]byte // 16 bits
+	qdcount uint16  // 16 bits
+	ancount uint16  // 16 bits
+	nscount uint16  // 16 bits
+	arcount uint16  // 16 bits
+}
+
+func NewDnsHeader() DnsHeader {
+	return DnsHeader{
+		id:      1234,
+		flags:   [2]byte{1 << 7, 0},
+		qdcount: 0,
+		ancount: 0,
+		nscount: 0,
+		arcount: 0,
+	}
+}
+
+func GenDnsHeaderResponse(hdr DnsHeader) []byte {
+	resp := []byte{
+		byte(hdr.id >> 8), byte(hdr.id), // split uint16 into two bytes
+		hdr.flags[0], hdr.flags[1],
+		byte(hdr.qdcount >> 8), byte(hdr.qdcount),
+		byte(hdr.ancount >> 8), byte(hdr.ancount),
+		byte(hdr.nscount >> 8), byte(hdr.nscount),
+		byte(hdr.arcount >> 8), byte(hdr.arcount),
+	}
+	return resp
+}
+
+// type DnsMessage struct {
+// 	hdr DnsHeader
+// }
